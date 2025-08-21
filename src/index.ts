bd67e14830c6413748adb5a5e5c9f56cb9aa9356
amandalyckenius <alyckenius@gmail.com>
2025-08-21 11:19:53 +0200

    Add: database connection methodsand update index.ts to use database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,49 @@
 import express from "express"
+import type {Request, Response} from "express"
+import "dotenv/config"
+import {closeDB, runDB} from "./database/database.js"
+
 
 const app = express()
 const port: number = 3000
 
-app.get("/", (req,res) => {
-    res.send("Hello World")
+
+app.get("/:id", (req: Request, res: Response) => {
+    const id: number = Number(req.params.id)
+
+    if (isNaN(id)){
+        res.status(400).send("not a number")
+        return
+    }
+    
+    res.send({ id: id })
+
 })
 
-app.listen(port, () => {
-    console.log("listening on port: " + port)
+
+
+async function startServer() {
+    try {
+        await runDB()
+        app.listen(port, () => {
+            console.log("listening on port: " + port)
+            console.log(`Start the app: http://localhost:${port}`)
+        })
+        process.on("SIGINT", async () => {
+            console.log("cleaning up..")
+            await closeDB()
+            process.exit(0)
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer()
+
+/*
+app.get("/", (req,res) => {
+    res.status(200).send({ message: "Hello world!" })
 })
+*/
